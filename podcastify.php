d7ef5833477d815b37b1b9d69b93937cdcc2b8e7
rmens <r.mens@me.com>
2016-01-15 23:34:31 +0100

    Fix typo

--- podcastify.php
+++ podcastify.php
@@ -1,8 +1,8 @@
 <?php
 
 add_filter( 'gettext', 'techsnacks_gettext', 10, 3 );
-function techsnacks_gettext($vertaald, $orineel, $domein) {
-	if($orineel === 'Reacties zijn afgesloten.') {
+function techsnacks_gettext($vertaald, $origineel, $domein) {
+	if($origineel === 'Comments are closed.') {
 		return 'Reageer op deze uitzending via het <a href=“http://techsnacks.nl/submit/>reactieformulier</a>.';
 	}
 	
