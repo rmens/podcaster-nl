4d1754e47ace90600fdef87e4d615175cfc4b010
rmens <r.mens@me.com>
2016-01-16 00:12:41 +0100

    Update mu-plugin

--- podcastify.php
+++ podcastify.php
@@ -1,12 +1,14 @@
 <?php
 
 /* Voor zijbalk op /blog/ (overschrijft wp-nl) */
-
 add_filter( 'gettext', 'techsnacks_gettext', 10, 3 );
 function techsnacks_gettext($vertaald, $origineel, $domein) {
 	if($origineel === 'Categories') {
-		return 'Thema'/s';
+		return 'Thema\'s';
 	}
-	
+/* Voor zijbalk op /blog/ (overschrijft wp-nl) */
+	if($origineel === 'Archives') {
+		return 'Maanden';
+	}
 	return $vertaald;
 }
