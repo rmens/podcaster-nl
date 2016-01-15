ef23e770a7e09c6f7e4485507685f98df3837324
rmens <r.mens@me.com>
2016-01-15 23:56:07 +0100

    Update mu-plugin and other translations

--- podcastify.php
+++ podcastify.php
@@ -1,9 +1,11 @@
 <?php
 
+/* Voor zijbalk op /blog/ (overschrijft wp-nl) */
+
 add_filter( 'gettext', 'techsnacks_gettext', 10, 3 );
 function techsnacks_gettext($vertaald, $origineel, $domein) {
-	if($origineel === 'Comments are closed.') {
-		return 'Reageer op deze uitzending via het <a href=“http://techsnacks.nl/submit/>reactieformulier</a>.';
+	if($origineel === 'Categories') {
+		return 'Thema'/s';
 	}
 	
 	return $vertaald;
